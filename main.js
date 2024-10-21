06466cfcb78c3025bda78766fa6cc042713d2b10
coddiinngg <oms0613@naver.com>
2024-10-21 15:51:46 +0900

    To do List

--- main.js
+++ main.js
@@ -1,79 +1,125 @@
-//업 앤 다운 게임
+//input에 내용을 받는다.
+//+를 눌러 내용을 task로 보낸다.
+//check를 누르면 삭선&회색 으로 됐다는 표시를 한다.
+//delete를 누르면 삭제되게 한다.
+//section에 따라 변하게한다.
+//input에 글씨가 없으면 button을 비활성화 시킨다.
 
-//제목
-//목숨칸
-//결과칸
-//입력칸
-//실행칸
-//리셋
+let addInput = document.getElementById("add-input");
+let addButton = document.getElementById("add-button");
+let taskSection = document.querySelectorAll("#task-section div");
+let slideLine = document.getElementById("slide-line");
+let filterList = [];
+let taskList = [];
+let selection = "all";
 
-//랜덤숫자
-//사용자 숫자
-//숫자 비교
-//리셋
-//목숨
-//숫자 범위
+const task = document.getElementById("task");
 
-let randomNum = 0;
-let userNum = document.getElementById("userNum");
-let playArea = document.getElementById("playArea");
-let resultArea = document.getElementById("resultArea");
-let chanceArea = document.getElementById("chanceArea");
-let resetArea = document.getElementById("resetArea");
-let chance = 5;
-let disabled = false;
-let userNumArr = [];
+addButton.addEventListener("click", pushTask);
 
-playArea.addEventListener("click", comparisonNum);
-resetArea.addEventListener("click", reset);
-userNum.addEventListener("click", function () {
-  userNum.value = "";
+addInput.addEventListener("input", eraser);
+addInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter" && addInput.value.trim() != "") {
+    pushTask();
+  }
 });
 
-function randomNumMk() {
-  randomNum = Math.floor(Math.random() * 100) + 1;
-  console.log(randomNum);
-}
-randomNumMk();
-
-function comparisonNum() {
-  if (userNum.value <= 0 || userNum.value > 100) {
-    resultArea.textContent = "1~100사이의 숫자를 입력해주세요!";
-    return;
-  }
-  if (userNumArr.includes(userNum.value)) {
-    resultArea.textContent = "이미 입력한 숫자입니다!";
-    return;
-  }
-
-  chance--;
-  chanceArea.textContent = `남은 기회 : ${chance}번`;
-
-  if (userNum.value > randomNum) {
-    resultArea.textContent = "DOWN!";
-  } else if (userNum.value < randomNum) {
-    resultArea.textContent = "UP!";
+function eraser() {
+  if (addInput.value.trim() != "") {
+    addButton.disabled = false;
   } else {
-    resultArea.textContent = "맞췄습니다!";
-    chanceArea.textContent = "!@#$%^&*()_+";
-    playArea.disabled = true;
-  }
-
-  userNumArr.push(userNum.value);
-
-  if (chance == 0) {
-    playArea.disabled = true;
-    chanceArea.textContent = "남은 기회가 없습니다!";
-    resultArea.textContent = "아쉽군요!";
-    userNum.value = "";
+    addButton.disabled = true;
   }
 }
 
-function reset() {
-  chance = 5;
-  chanceArea.textContent = "남은 기회 : 5번";
-  resultArea.textContent = "시작 해볼까요?";
-  playArea.disabled = false;
-  userNum.value = "";
-  randomNumMk();
+for (let i = 1; i < taskSection.length; i++) {
+  taskSection[i].addEventListener("click", section);
+}
+
+function pushTask() {
+  taskContent = {
+    value: addInput.value,
+    IsComplete: false,
+    id: randomId(),
+  };
+  taskList.push(taskContent);
+  console.log(taskList);
+  render();
+  addInput.value = "";
+  addButton.disabled = true;
+}
+
+function render() {
+  let list = [];
+  if (selection === "all") {
+    list = taskList;
+  } else if (selection === "done" || selection === "doing") {
+    list = filterList;
+  }
+  let result = "";
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].IsComplete === false) {
+      result += `<div class="task-content">
+      <div class="task-width">${list[i].value}</div>
+      <div div class="check-button">
+        <button onclick="taskComplete ('${list[i].id}')"><i class="fa-solid fa-check"></i></button>
+        <button onclick="taskDelete ('${list[i].id}')"><i class="fa-solid fa-trash"></i></button>
+      </div>
+      </div>`;
+    } else if (list[i].IsComplete === true) {
+      result += `<div class="task-content">
+      <div class="task-complete task-width">${list[i].value}</div>
+      <div class="check-button">
+        <button onclick="taskComplete ('${list[i].id}')"><i class="fa-solid fa-check"></i></button>
+        <button onclick="taskDelete ('${list[i].id}')"><i class="fa-solid fa-trash"></i></button>
+      </div>
+      </div>`;
+    }
+  }
+  task.innerHTML = result;
+}
+
+function taskComplete(event) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (event === taskList[i].id) {
+      taskList[i].IsComplete = !taskList[i].IsComplete;
+    }
+  }
+  render();
+}
+
+function taskDelete(event) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (event === taskList[i].id) {
+      taskList.splice(i, 1);
+    }
+  }
+  render();
+  console.log(taskList);
+}
+
+function section(event) {
+  selection = event.target.id;
+  filterList = [];
+  if (selection === "all") {
+    render();
+  } else if (selection === "done") {
+    for (i = 0; i < taskList.length; i++) {
+      if (taskList[i].IsComplete === true) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  } else if (selection === "doing") {
+    for (i = 0; i < taskList.length; i++) {
+      if (taskList[i].IsComplete === false) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  }
+}
+
+function randomId() {
+  return "_" + Math.random().toString(36).substr(2, 9);
 }
